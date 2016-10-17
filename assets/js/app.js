62762371983a437b4b5cda91016f6a2ef209dcef
Austin Eady <austineady@gmail.com>
2016-10-17 18:26:34 -0400

    Got slick downloaded and more of the page built

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
 $(document).foundation()
+
+$(document).ready(function() {
+  $('#js-product-carousel').slick({
+  arrows: false,
+  centerMode: true,
+  centerPadding: '150px',
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 800,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        slidesToScroll: 3
+      }
+    }
+  ]
+});
+})
