85c24739c63a7abcce535384b97125d4779478ac
Austin Eady <austineady@gmail.com>
2016-10-21 16:31:35 -0400

    Adding in typed and coding for mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,51 @@ $(document).ready(function() {
         slidesToShow: 1,
         slidesToScroll: 3
       }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '30px',
+        slidesToShow: 1,
+        slidesToScroll: 3
+      }
     }
   ]
 });
+$('.js-logo-bar').slick({
+  arrows: false,
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  variableWidth: true,
+  variableHeight: true,
+  responsive: [
+    {
+      breakpoint: 500,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        cssEase: 'ease-out'
+      }
+    }
+  ]
+});
+
+function startTextRotation() {
+  var $textHook = $(".js-text-hook");
+  $textHook.typed({
+    strings: ["t-shirts", "jackets", "hoodies"],
+    typeSpeed: 100,
+    loop: true,
+    showCursor: false
+  });
+}
+
+startTextRotation();
 })
