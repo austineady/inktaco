213c5394868071c8b152fe453d4b44c7760e4a6d
Austin Eady <austineady@gmail.com>
2016-11-09 17:50:19 -0500

    More feedback edits

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,110 +1,139 @@
-$(document).foundation()
+$(document).foundation();
 
 $(document).ready(function() {
-  $('#js-product-carousel').slick({
-  arrows: false,
-  centerMode: true,
-  centerPadding: '350px',
-  slidesToShow: 1,
-  initialSlide: 2,
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        centerPadding: '300px'
+init();
+function init() {
+    $('#js-product-carousel').html("");
+    var productImageLength = 24;
+    var data = Array(productImageLength).fill({
+      "title": "VentureApp",
+      "type": "Charcoal Black Triblend",
+      "details": "3 color imprint"
+    }, 0, productImageLength);
+    console.log(data);
+    render(data);
+  }
+
+  function render(data) {
+    data.forEach(function(product, index) {
+      var prevArrow = $('<div class="icon-arrow-prev js-prev-product"></div>');
+      var nextArrow = $('<div class="icon-arrow-next js-next-product"></div>');
+      var repeatArrow = $('<div class="fa fa-repeat icon-arrow-repeat js-first-product"></div>');
+      var item = $('<div class="product"><img src="assets/images/product-image-'+ (index + 1) +'.png" alt="'+product.title+' Product" class="product__image"><div class="product__description"><h4 class="product__title">'+product.title+'</h4><h5 class="product__type">'+product.type+'</h5><h5 class="product__details">'+product.details+'</h5><a href="mailto:andy@inktaco.com?subject=Interested&nbsp;in&nbsp;Custom&nbsp;Apparel" class="product__contact">Contact us about a similar product</a></div></div>');
+      if(index < 1) {
+        item.append(nextArrow);
+      } else if(index + 1 == data.length) {
+        item.prepend(prevArrow);
+        item.append(repeatArrow);
+      } else {
+        item.prepend(prevArrow);
+        item.append(nextArrow);
       }
-    },
-    {
-      breakpoint: 1100,
-      settings: {
-        centerPadding: '250px'
-      }
-    },
-    {
-      breakpoint: 980,
-      settings: {
-        centerPadding: '200px'
-      }
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        centerPadding: '150px'
-      }
-    },
-    {
-      breakpoint: 800,
-      settings: {
-        arrows: false,
-        centerMode: false,
-        centerPadding: '80px',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        cssEase: 'ease-out'
-      }
-    },
-    {
-      breakpoint: 450,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '50px',
-        slidesToShow: 1
-      }
-    }
-  ]
-});
-$('.js-prev-product').on('click', function() {
-  $('#js-product-carousel').slick('slickPrev');
-});
-$('.js-next-product').on('click', function() {
-  $('#js-product-carousel').slick('slickNext');
-});
-if($('.js-logo-bar').width() < 950) {
-$('.js-logo-bar').slick({
-  arrows: false,
-  slidesToShow: 5,
-  slidesToScroll: 3,
-  variableWidth: true,
-  variableHeight: true,
-  responsive: [
-    {
-      breakpoint: 950,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        initialSlide: 2,
-        slidesToShow: 1,
-        slidesToScroll: 3,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        cssEase: 'ease-out'
-      }
-    },
-    {
-      breakpoint: 500,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        initialSlide: 2,
-        slidesToShow: 1,
-        slidesToScroll: 3,
-        cssEase: 'ease-out'
-      }
-    }
-  ]
-});
-}
+      $('#js-product-carousel').append(item);
+    }) // end forEach
+    $('#js-product-carousel').addClass('is-active');
+    $('#js-product-carousel').slick({
+      arrows: false,
+      centerMode: true,
+      centerPadding: '325px',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      infinite: false,
+      variableWidth: true,
+      speed: 600,
+      responsive: [
+        {
+          breakpoint: 1000,
+          settings: {
+            centerPadding: '100px'
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '100px',
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            cssEase: 'ease-out'
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '0px',
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            cssEase: 'ease-out'
+          }
+        }
+      ]
+    });
+
+    $('.js-prev-product').on('click', function() {
+      $('#js-product-carousel').slick('slickPrev');
+    });
+    $('.js-next-product').on('click', function() {
+      $('#js-product-carousel').slick('slickNext');
+    });
+    $('.js-first-product').on('click', function() {
+      $('#js-product-carousel').slick('slickGoTo', 0, false);
+    })
+  }
+
+  if($('.js-logo-bar').width() < 950) {
+    $('.js-logo-bar').slick({
+      arrows: false,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      variableWidth: true,
+      variableHeight: true,
+      responsive: [
+        {
+          breakpoint: 950,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            initialSlide: 2,
+            slidesToShow: 1,
+            slidesToScroll: 3,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            cssEase: 'ease-out'
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            initialSlide: 2,
+            slidesToShow: 1,
+            slidesToScroll: 3,
+            cssEase: 'ease-out'
+          }
+        }
+      ]
+    });
+  }
 
 (function startTextRotation() {
   var $textHook = $(".js-text-hook");
   $textHook.typed({
     strings: ["t-shirts", "sweatshirts", "zip ups", "tank tops", "long sleeves", "v-necks", "hoodies", "polos", "apparel"],
-    typeSpeed: 75,
+    typeSpeed: 40,
     backSpeed: 60,
     startDelay: 200,
     backDelay: 300,
@@ -113,20 +142,6 @@ $('.js-logo-bar').slick({
     showCursor: false
   });
 })()
-
-$('.js-modal-toggle').on('click', function(e) {
-  $('.modal').addClass('is-active');
-  $('body').addClass('modal-active');
-})
-
-$('.modal').on('click', function(e) {
-  $(this).removeClass('is-active');
-  $('body').removeClass('modal-active');
-})
-
-$('.modal__content').on('click', function(e) {
-  e.stopPropagation();
-})
 
 
 var scrollMem = 0;
